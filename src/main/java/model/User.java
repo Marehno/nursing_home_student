0c595349ce995b978e1a15d143f92298db257dbd
Marehno <44499113+Marehno@users.noreply.github.com>
2022-06-27 15:17:29 +0200

    Update User.java

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,5 +1,46 @@
 package model;
 
-public class User {
+/**
+ * The User has accsess to the programm
+ */
+public class User extends Person {
+    private long uid;
+    private String username;
+    private String password;
+
+
+    public User(String firstName, String surname, String username, String password) {
+        super(firstName, surname);
+        this.username = username;
+        this.password = password;
+    }
+
+    public User(long uid, String firstName, String surname, String username, String password) {
+        super(firstName, surname);
+        this.uid = uid;
+        this.username = username;
+        this.password = password;
+    }
+
+
+    public long getUid() {
+        return uid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
 }
